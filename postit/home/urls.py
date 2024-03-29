579ad69c3e483aa3cfcc6390bb8b62f13aa6aa51
Jansxn <jason.dmello21@gmail.com>
2024-03-29 12:09:34 +0530

    Added pdf previews with jquery and indivial postbox pages

--- postit/home/urls.py
+++ postit/home/urls.py
@@ -1,7 +1,10 @@
-from django.urls import path
+from django.urls import path, re_path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-]
+    re_path(r'^(?!login$|signup$|logout$)([a-zA-Z0-9_]+)/$', views.postbox, name='postbox'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
