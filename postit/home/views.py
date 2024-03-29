7af96b6d9d7e8c77bcac2ffda5c73d55f7a0f416
Jason Shaun DMello <jason.dmello21@gmail.com>
2024-03-29 15:59:33 +0530

    Major update. UI done (#6)
    * Updated login page design
    
    * Started work on main page.
    
    * Designed homepage
    
    * Added pdf previews with jquery and indivial postbox pages
    
    * Added box for users to post in

--- postit/home/views.py
+++ postit/home/views.py
@@ -3,3 +3,12 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, "home.html")
+
+def postbox(request, postbox_name):
+    postbox_description = "This is a postbox for " + postbox_name
+    # post = Post.objects.get(file_name=postbox_name)
+    return render(request, "postbox.html", {
+        "postbox_name": postbox_name,
+        "postbox_description": postbox_description
+    })
+    
