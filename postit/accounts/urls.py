e269d12bbbec5df3834df1c343a093f0bc588cb4
Himanshu Banerji <himanshubanerji@gmail.com>
2024-03-30 22:35:00 +0530

    accounts urls/py changed for macOs routing

--- postit/accounts/urls.py
+++ postit/accounts/urls.py
@@ -1,11 +1,9 @@
-
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
     # path("", views.index, name="login"),
-    path("signup", views.signup, name="signup"),
-    path("login", views.login, name="login"),
-    path("logout", views.logout, name="logout")
+    path("signup/", views.signup, name="signup"),
+    path("login/", views.login, name="login"),
+    path("logout/", views.logout, name="logout")
 ]
